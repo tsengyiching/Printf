abc947696adc4e2f744efb8d82f7b873c4f6255d
Yiching Tseng <yictseng@z4r8p2.le-101.fr>
2019-12-20 22:07:47 +0100

    c s d

---
+++ printf.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   printf.c                                         .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: yictseng <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/12/14 17:35:01 by yictseng     #+#   ##    ##    #+#       */
+/*   Updated: 2019/12/14 17:37:24 by yictseng    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "printf_libft.h"
+#include "libft/libft.h"
+
+void	printf_char(va_list *ap)
+{
+	char c;
+
+	c = va_arg(*ap, int);
+	write(1, &c, 1);
+}
+
+void	printf_str(va_list *ap)
+{
+	char *str;
+
+	str = va_arg(*ap, char *);
+	write(1, str, ft_strlen(str));
+}
+
+void	printf_nbr(va_list *ap)
+{
+	int nb;
+
+	nb = va_arg(*ap, int);
+	ft_putnbr_fd(nb, 1);
+}
+
+int		find_index(char *tab, char element)
+{
+	int index;
+
+	index = 0;
+	while (tab[index])
+	{
+		if (tab[index] == element)
+			return (index);
+		index++;
+	}
+	return (-1);
+}
+
+int		ft_printf(const char *format, ...)
+{
+	void (*tab_function[3]) (va_list *);
+	char	tab_index[4];
+	va_list ap;
+	int 	pos;
+	int		temp;
+
+	pos = 0;
+	temp = 0;
+	tab_index[0] = 'c';
+	tab_index[1] = 's';
+	tab_index[2] = 'd';
+	tab_index[3] = 0;
+	tab_function[0] = printf_char;
+	tab_function[1] = printf_str;
+	tab_function[2] = printf_nbr;
+	va_start(ap, format);
+	while (format[pos])
+	{
+		if (pos != 0 && format[pos - 1] == '%')
+		{
+			temp = find_index(tab_index, format[pos]);
+			if (temp != -1)
+				(*tab_function[temp]) (&ap);
+		}
+		else
+			write(1, &format[pos], 1);
+		pos++;
+	}
+	va_end(ap);
+	return (0);
+}
---
+++ printf_libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   printf_libft.h                                   .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: yictseng <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/12/16 19:14:23 by yictseng     #+#   ##    ##    #+#       */
+/*   Updated: 2019/12/16 19:16:47 by yictseng    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef PRINTF_LIBFT_H
+# define PRINTF_LIBFT_H
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
+
+typedef struct		s_struct
+{
+    int             left_justify;
+    int             field;
+    int             width;
+    int             precision;
+    int             add_integer;
+}					t_struct;
+
+int		ft_printf(const char *format, ...);
+#endif
